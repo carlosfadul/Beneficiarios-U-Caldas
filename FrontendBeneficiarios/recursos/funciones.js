e3421e13b2857d052a3f935c57b1b19932b8df64
Carlos Fadul <carlosfadul@gmail.com>
2022-03-02 09:36:16 -0500

    tercer envío

--- FrontendBeneficiarios/recursos/funciones.js
+++ FrontendBeneficiarios/recursos/funciones.js
@@ -1,61 +1,142 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }else{
-              RegistrarBeneficiario();
-              event.preventDefault()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
 
-  function RegistrarBeneficiario(){
-    let identificacion = document.querySelector("#validacionIdentificacion").value;
-    let documentoIgual = document.querySelector("#validacionIgualMinTic").value;
-    let fechaNac = document.querySelector("#validacionFechaNac").value;
-    let mayor15 = document.querySelector("#validacionMayor15").value;
-    let colombiano = document.querySelector("#validacionColombiano").value;
-    let bachiller = document.querySelector("#validacionBachiller").value;
-    let cumpleReq = document.querySelector("#validacionCumple").value;
-    let revisor = document.querySelector("#validacionRevisor").value;
-    let observaciones = document.querySelector("#validacionObservaciones").value;
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+  let encontrado = true
 
-    let url = `http://localhost:3000/beneficiarios`;
-    let datos = {
-        identificacion: identificacion,
-        documentoIgual: documentoIgual,
-        fechaNac: fechaNac,
-        mayor15: mayor15,
-        colombiano: colombiano,
-        bachiller: bachiller,
-        cumpleReq: cumpleReq,
-        revisor: revisor,
-        observaciones: observaciones
-    };
-
-    
-
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(datos),
-        headers:{
-            'Content-Type':'application/json'
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        } else {
+          BuscarBeneficiario();
+          event.preventDefault()
         }
-    }).then(res => res.json())
-    .then(mensaje => {
-        console.log(mensaje)
+
+        form.classList.add('was-validated')
+      }, false)
     })
+})()
+
+function Validar() {
+
+  let fecha = document.querySelector("#txtFechaNac").value;
+  let edad = calcularEdad(fecha)
+  if (edad > 14)
+    document.getElementById("txtMayor15").value = "Si";
+  else
+    document.getElementById("txtMayor15").value = "No";
+  let mayor15 = document.querySelector("#txtMayor15").value;
+  let colombiano = document.querySelector("#txtColombiano").value;
+  let bachiller = document.querySelector("#txtBachiller").value;
+
+  if (mayor15 == "Si" && colombiano == "Si" && bachiller == "Si")
+    document.getElementById("txtCumple").value = "Si";
+  else
+    document.getElementById("txtCumple").value = "No";
+}
+
+function calcularEdad(fecha) {
+  var hoy = new Date();
+  var cumpleanos = new Date(fecha);
+  var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+  var m = hoy.getMonth() - cumpleanos.getMonth();
+
+  if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+    edad--;
   }
+
+  return edad;
+}
+
+
+function BuscarBeneficiario() {
+  let identificacion = document.querySelector("#txtIdentificacion").value;
+  let documentoIgual = document.querySelector("#txtIgualMinTic").value;
+  let fechaNac = document.querySelector("#txtFechaNac").value;
+  let mayor15 = document.querySelector("#txtMayor15").value;
+  let colombiano = document.querySelector("#txtColombiano").value;
+  let bachiller = document.querySelector("#txtBachiller").value;
+  let cumpleReq = document.querySelector("#txtCumple").value;
+  let revisor = document.querySelector("#txtRevisor").value;
+  let observaciones = document.querySelector("#txtObservaciones").value;
+
+  let url = `http://localhost:3000/beneficiarios/?filter={"where":{"identificacion":${identificacion}}}`;
+
+  let datos = {
+    identificacion: identificacion,
+    documentoIgual: documentoIgual,
+    fechaNac: fechaNac,
+    mayor15: mayor15,
+    colombiano: colombiano,
+    bachiller: bachiller,
+    cumpleReq: cumpleReq,
+    revisor: revisor,
+    observaciones: observaciones
+  };
+
+
+
+  fetch(url, {
+    method: 'GET',
+
+    headers: {
+      'Content-Type': 'application/json'
+    }
+
+  }).then(res => res.json())
+    .then(mensaje => {
+
+      console.log(mensaje)
+      if (mensaje != "")
+        alert("Este beneficiario ya fue procesado")
+      else {
+        alert("Beneficiario agregado a la base de datos")
+        RegistrarBeneficiario()
+      }
+    })
+}
+
+function RegistrarBeneficiario() {
+  let identificacion = document.querySelector("#txtIdentificacion").value;
+  let documentoIgual = document.querySelector("#txtIgualMinTic").value;
+  let fechaNac = document.querySelector("#txtFechaNac").value;
+  let mayor15 = document.querySelector("#txtMayor15").value;
+  let colombiano = document.querySelector("#txtColombiano").value;
+  let bachiller = document.querySelector("#txtBachiller").value;
+  let cumpleReq = document.querySelector("#txtCumple").value;
+  let revisor = document.querySelector("#txtRevisor").value;
+  let observaciones = document.querySelector("#txtObservaciones").value;
+
+  let url = `http://localhost:3000/beneficiarios`;
+  let datos = {
+    identificacion: identificacion,
+    documentoIgual: documentoIgual,
+    fechaNac: fechaNac,
+    mayor15: mayor15,
+    colombiano: colombiano,
+    bachiller: bachiller,
+    cumpleReq: cumpleReq,
+    revisor: revisor,
+    observaciones: observaciones
+  };
+
+
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(mensaje => {
+      console.log(mensaje)
+      alert("Beneficiario almacenado correctamente")
+    })
+}
